5ee2866b8ad6ea84c0a6d246e59798e33ad4fe06
amirhossein33 <shiravandispdf@gmail.com>
2025-05-03 23:16:39 +0330

    Commit

---
+++ Domain/Entities/Friend.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Friend
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string EmailAddress { get; private set; }
+        public string Phone { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string CurrentCity { get; private set; }
+    }
+}
---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities
+{
+    public class Post
+    {
+        public Guid PostId { get; private set; }
+        public string Text { get; private set; }
+    }
+}
